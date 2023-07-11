f4cc56aba9dcb1bb09c350e2e1e0e15f7a41a9a7
Jose Melero Martinez <jmelero08@gmail.com>
2023-07-11 17:15:28 -0600

    Added users with identity scaffold

---
+++ MyCarReviewMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCarReviewMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
